384602b31568e375fc24bb970cb633eca534f933
bitcoineazy <70742214+bitcoineazy@users.noreply.github.com>
2020-09-13 22:32:12 +0300

    Yandex_praktikum_13.09

---
+++ Объединение_строк_метод_join().py
@@ -0,0 +1,3 @@
+friends = ['Sergey','Sonya','Dima','Alina']
+friends_string = ', '.join(friends)
+print('Твои друзья:',friends_string)
---
+++ работа_со_строками.py
@@ -0,0 +1,3 @@
+temperature_exact = '39.3' # (жара)
+temperature_approx = temperature_exact     # преобразуйте значение в целое и прибавьте 1
+print('За окном', temperature_exact , 'градусов Цельсия.', 'Это почти' , str(temperature_approx))
