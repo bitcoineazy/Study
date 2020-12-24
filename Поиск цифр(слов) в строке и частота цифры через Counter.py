e52f27b94236142e24f905e18694d24df8fd799d
Matvey <bitcoineazy@gmail.com>
2020-12-24 20:25:41 +0300

    collections Counter, поиск цифр(слов) в строке и их частота

---
+++ Поиск цифр(слов) в строке и частота цифры через Counter.py
@@ -0,0 +1,27 @@
+'''Реализуйте функцию, для поиска в строке цифр,
+ представленных в виде цифр или слов, для встречи
+ которых в любом виде составляется словарь, ключами
+  которого являются все цифры, а значениями число упоминаний
+   о них в строке в любом виде. Проверить работу функции при произвольных параметрах.'''
+import re
+from collections import Counter
+
+
+str1 = '1 2 3 2 1 3 one two three four'
+def func(str1):
+    num = []
+    num_dict = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+                'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0}
+    for i in str1:
+        if i.isdigit():
+            num.append(i)
+    pattern = re.findall('\w+', str1)
+    print(pattern)
+    for each in pattern:
+        if each in num_dict.keys():
+            num.append(str(num_dict[each]))
+    output = ''.join(num)
+    c = Counter(zip(output))
+    print(c)
+
+func(str1)
