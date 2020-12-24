22e1863c092718fd4bbcce74c5c86e8a6583ec3f
Matvey <bitcoineazy@gmail.com>
2020-12-25 01:42:11 +0300

    Kivy - обработка событий, новые свойства, методы, ObjectProperty()

--- Kivy_уроки/Kivy_обработка_событий.py
+++ Kivy_уроки/Kivy_обработка_событий.py
@@ -1,10 +1,17 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-
+from kivy.properties import ObjectProperty
 
 class Container(GridLayout):
-    pass
+    #назначаем нормальные свойства, импортировав ObjectProperty
+    #ObjectProperty - создает ссылку на объект в питоне(почти для всех типов данных)
+    text_input = ObjectProperty()
+    label_widget = ObjectProperty()
+    def change_text(self):
+        #ставим текст у Label = тексту у TextInput
+        #используя указанные нами новые свойства
+        self.label_widget.text = self.text_input.text
 
 
 class EventApp(App):
