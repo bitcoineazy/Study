1655fd1faf5d08be5f6159b2a1341215ec0e8823
Matvey <bitcoineazy@gmail.com>
2021-05-28 19:30:59 +0300

    almost

--- Алгоритмы/сортировка_пути_2.py
+++ Алгоритмы/сортировка_пути_2.py
@@ -1,5 +1,5 @@
-x = 14
-y = '0 3 41 0 0 0 0 0 49 0 0 56 0 88'.split(' ') # answer: 0 1 2 1 0
+x = 20
+y = '3 15 0 22 31 32 0 41 0 0 50 0 0 66 0 76 77 82 0 89'.split(' ') # answer: 0 1 2 1 0
 #x = int(input())
 #y = input().split(' ')
 
@@ -12,18 +12,17 @@ def sort_alg(n, points):
             break
     for i in reversed(points):
         if i == 0:
-            zero_indexes.append((len(points) - points.index(i)) - 1)
+            zero_indexes.append((len(points) - points.index(i)) - 2)
             break
-    print(zero_indexes)
+    #print(zero_indexes)
     output = []
     for i in range(n):
         output.append(0)
-    for i in range(zero_indexes[0], len(output)):
+    for i in range(zero_indexes[0], n):
         if points[i] == 0:
             output[i] = 0
         else:
             output[i] = output[i-1] + 1
-    print(points)
     for i in range(zero_indexes[1], zero_indexes[0], -1):
         if points[i] == 0:
             output[i] = 0
