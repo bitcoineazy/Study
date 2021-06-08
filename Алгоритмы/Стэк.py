1c79f4aa86346e08c7c6cdf2140b27a49f03f1d7
noble6 <bitcoineazy@gmail.com>
2021-06-09 01:27:08 +0300

    Стэк

---
+++ Алгоритмы/Стэк.py
@@ -0,0 +1,34 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(int(item))
+
+    def pop(self):
+        if len(self.items) > 0:
+            return self.items.pop()
+        else:
+            print('error')
+
+    def get_max(self):
+        if len(self.items) > 0:
+            print(max(i for i in self.items))
+        else:
+            print('None')
+
+
+def main():
+    n = int(input())
+    commands = [input().split() for i in range(n)]
+    stack = Stack()
+    for i in commands:
+        if len(i) > 1:
+            stack.push(i[1])
+        if 'pop' in i:
+            stack.pop()
+        if 'get_max' in i:
+            stack.get_max()
+
+
+main()
